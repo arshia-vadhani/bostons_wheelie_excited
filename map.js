7aeb11294897183a5d87439b7e21ea23d7e1efa5
arshia-vadhani <130110649+arshia-vadhani@users.noreply.github.com>
2025-02-20 23:07:04 -0800

    Update map.js

--- map.js
+++ map.js
@@ -31,7 +31,7 @@ map.on('load', () => {
     // Add Boston bike lanes
     map.addSource('boston_route', {
         type: 'geojson',
-        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson'
+        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?...'
     });
 
     map.addLayer({
